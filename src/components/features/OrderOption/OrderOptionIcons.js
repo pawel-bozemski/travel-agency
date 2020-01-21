dba984502aff8c755454334e1b0cd4af11b4160e
Pawel Bozemski <paw.boz@gmail.com>
2020-01-21 22:15:41 +0100

    Make optionIcons and find bug with class adding

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,16 +1,40 @@
 import React from 'react';
-// import styles from './OrderOption.scss';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+import {formatPrice} from '../../../utils/formatPrice';
+import Icon from '../../common/Icon/Icon';
 
-const OrderOptionIcons =() => (
 
-  <div>
-  OrderOptionIcons
+const OrderOptionIcons = ({values, required, currentValue, setOptionValue}) => (
+
+  <div className={styles.icon}>
+    {required ? '' : (
+      <div onClick={() => setOptionValue('')}>
+        <Icon name='times-circle' /> NONE
+      </div>
+    )}
+    {values.map(value => (
+      <div
+        className={styles.icon + (currentValue === value.id ? styles.iconActive : '')}
+        key={value.id}
+        onClick={() => setOptionValue(value.id)}
+      >
+        {console.log('value.id', value.id),
+        console.log('currentValue', currentValue)}
+        <Icon name={value.icon}></Icon> {value.name} ({formatPrice(value.price)})
+      </div>
+    ))}
+
   </div>
 
 );
 
-
-
+OrderOptionIcons.propTypes = {
+  values: PropTypes.array,
+  currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
+  required: PropTypes.bool,
+};
 
 export default OrderOptionIcons;
 
