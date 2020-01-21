748a5cd4d3a7b3312e6d6676ff084a084808d33f
Pawel Bozemski <paw.boz@gmail.com>
2020-01-21 20:58:17 +0100

    Add OrderForm and displaying option for each trip

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import OrderOption from '../OrderOption/OrderOption';
 import PropTypes from 'prop-types';
+import pricing from '../../../data/pricing.json';
 
 const OrderForm =(props) => (
 
   <Row>
+    {pricing.map(option =>
+      <Col md={4} key={option.id}>
+        <OrderOption title={option.name}/>
+      </Col>)}
     <Col xs={12}>
       <OrderSummary cost={props.tripCost} options={props.options}/>
     </Col>
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+
+const OrderOption =(props) => (
+
+  <div className={styles.component}>
+    <h3 className={styles.title}>{props.title}</h3>
+  </div>
+
+);
+
+OrderOption.propTypes = {
+  title: PropTypes.string,
+};
+
+
+
+export default OrderOption;
+
+
+
