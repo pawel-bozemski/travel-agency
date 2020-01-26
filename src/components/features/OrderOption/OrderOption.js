d1c1d7c6edc2de74c1d4787723d3018932c45c99
Pawel Bozemski <paw.boz@gmail.com>
2020-01-26 11:34:42 +0100

    Add OptionDate and Text & finish module 18

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -5,6 +5,8 @@ import OrderOptionDropdown from './OrderOptionDropdown';
 import OrderOptionIcons from './OrderOptionIcons';
 import OrderOptionCheckboxes from './OrderOptionCheckboxes';
 import OrderOptionNumber from './OrderOptionNumber';
+import OrderOptionText from './OrderOptionText';
+import OrderOptionDate from './OrderOptionDate';
 
 
 const optionTypes = {
@@ -12,6 +14,8 @@ const optionTypes = {
   icons: OrderOptionIcons,
   checkboxes: OrderOptionCheckboxes,
   number: OrderOptionNumber,
+  text: OrderOptionText,
+  date: OrderOptionDate,
 };
 
 const OrderOption = ({name, type, id, setOrderOption, ...otherProps}) => {
---
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+
+const OrderOptionText = () => (
+
+  <input
+    className={styles.input}
+    type='text'
+  />
+);
+
+
+export default OrderOptionText;
