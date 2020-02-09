be926c9415cc6177ad415d7e3038502b4428da1e
Pawel Bozemski <paw.boz@gmail.com>
2020-02-09 15:20:14 +0100

    Add 1/7 test to HappyHourAd

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import styles from './HappyHourAd.scss';
+
+class HappyHourAd extends React.Component {
+
+  render(){
+    return(
+      <h3>gg</h3>
+    );
+  }
+
+}
+
+export default HappyHourAd;
+
